b818b7e2678bad58b866158dbd3a360a162e9df6
beb <b>
2020-06-16 13:15:30 +0200

    RGB221 added

--- colors.py
+++ colors.py
@@ -8,6 +8,7 @@ import time
 
 hh = ["00","55","aa","ff"]
 
+print("<table>RGB222")
 for r in range(4):
   print("<tr>")
   for g in range(4):
@@ -16,7 +17,21 @@ for r in range(4):
       print('<th style="background-color: '+ ch +'">%i%i%i</th>'%(r,g,b))
     print("\r\n")
   print("</tr>\r\n")
+print("</table>")
+ 
+ 
+print("<table>RGB221")
+for r in range(4):
+  print("<tr>")
+  for g in range(4):
+    for b in range(2):
+      ch = hh[r] +hh[g] +hh[b*3]
+      print('<th style="background-color: '+ ch +'">%i%i%i</th>'%(r,g,b))
+  print("</tr>\r\n")
+print("</table>")
  
  
 while(1):
   time.sleep(.1)
+  
+#eof
