d1d687dabe7b039aeb3871bef125a8167696a764
rundekugel <rundekugel@users.noreply.github.com>
2022-09-23 11:57:00 +0200

    title added

--- gitinfo.sh
+++ gitinfo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # by rundekugel-lifesim.de 
-# show pupup with git repo infos. add to thunar for your comfort.
 text=$(git log --date=iso  -n 1  --format=format:"Date: %ad%nHash: %h%n")
 text2=$text"\n"$(git remote -v)
+text2=$text2"\n\nby rundekugel-lifesim.de"
 # echo $text2
-zenity --info --text="$text2"
+zenity --info --title="GIT infos" --text="$text2" 
+
