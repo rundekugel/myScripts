0ea2a230121c366c9d225e913e2d876034468d36
beb <bressan@stepover.de>
2022-10-19 21:15:35 +0200

    gitinfo shows also branch

--- gitinfo.sh
+++ gitinfo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # by rundekugel-lifesim.de 
-text=$(git log --date=iso  -n 1  --format=format:"Date: %ad%nHash: %h%n")
+text=$(git log --date=iso  -n 1  --format=format:"Date: %ad%nHash: %h%n%d%n")
 text2=$text"\n"$(git remote -v)
 text2=$text2"\n\nby rundekugel-lifesim.de"
-# echo $text2
-zenity --info --title="GIT infos" --text="$text2" 
 
+zenity --info --title="GIT infos" --text="$text2" 
+# notify-send "GIT infos" "$text2" 
