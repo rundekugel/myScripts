c97a9cb09306f45b4eef22e8a31526cead0bdf88
beb <bressan@stepover.de>
2023-01-31 10:05:27 +0100

    chenges for ubuntu22

--- install-monodevelop.sh
+++ install-monodevelop.sh
@@ -6,17 +6,20 @@ apt update
 sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common -y
 
 echo install sources...
-sudo apt-add-repository 'deb https://download.mono-project.com/repo/ubuntu stable-focal main'
+# this was before ubuntu 22: sudo apt-add-repository 'deb https://download.mono-project.com/repo/ubuntu stable-focal main'
+echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-vs.list
 # this was before ubuntu 22: sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF  
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-sudo apt update
 
+sudo apt update
+sudo apt install apt-transport-https dirmngr
+
+sudo apt update
 echo monodevelop...
-sudo apt install mono-complete -y
-# sudo apt install monodevelop
+sudo apt install -y mono-complete monodevelop 
 
 echo versioncontrol...
-sudo apt install  monodevelop-versioncontrol -y
+sudo apt install  monodevelop-versioncontrol 
 
 echo done.
 ping ::1 -c 3
