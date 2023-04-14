e5d0b872186ff8c962d9d5d7561644f9537d47f9
rundekugel <rundekugel@gmx.de>
2023-04-15 00:30:33 +0200

    check for extension already configured

--- createXfceMimeType.py
+++ createXfceMimeType.py
@@ -9,7 +9,7 @@ params:
 -e=<fileextension>   the fileextension which shall be opened
 -f=<0|1>             force overwriting existing files. Be careful!
 -i                   interactive
--p=<vendor-prefix>   default=extension
+-p=<vendor-prefix>   default=ext
 -t=<filetype>        new name for a filetype
 """
 
@@ -26,14 +26,14 @@ text/x-myvendor-mytype=myapplication.desktop
 
 import sys,os
 
-__version__="0.0.3"
+__version__="0.1.0"
 __revision__="1"
 
 
 class globs:
    verbosity=1
    fileextension=None
-   vendor="extension"
+   vendor="ext"
    filetype=None
    apppath=None
    comment=""
@@ -53,19 +53,43 @@ def registerMimeType(fileextension=None, vendor="extension", filetype=None, comm
    :param force: 1=overwrite existing file
    :return: 0=ok
    """
+   # search for fileext. in xml files first. 
+   while fileextension[0] == ".":
+      fileextension=fileextension[1:]
+   fn = None
+   for p in ["~/.local/share/mime/packages/", "/usr/share/mime/packages/"]:
+     p=os.path.expanduser(p)  
+     for fil in os.listdir(p):  # scan files
+       fil = p+os.sep+fil
+       if fil.lower()[-4:]==".xml":
+         with open(fil,"r") as f:   # search for fileext
+           c = f.read()
+           if f'"*.{fileextension}"' in c.replace(" ",""):
+             # read mimetype from this file out of <mime-type type="application/x-extension-fcstd">
+             k = c.split("type=",1)[1:]
+             if not k: continue
+             c = k[0].split(">")[0]
+             if c[-1]=="/": c=c[-1:]
+             if not c: continue
+             if not force:
+               if globs.verbosity:
+                 print(f"Filetype {filetype} changed to existing {c}.")
+               filetype = c
+               fn = fil
+             break
+   
    if not filetype:
       raise Exception("Filetype must be set!")
    if not fileextension:
       raise Exception("Fileextension must be set!")
-   while fileextension[0] == ".":
-      fileextension=fileextension[1:]
-   fn = "/tmp/" + vendor +"-"+ filetype +".xml"
-   if not force and os.path.isfile(fn):
-      raise Exception(f"File {fn} exists, already!")
-   if globs.verbosity:
-      print(f"create file: {fn}...")
-   with open(fn,"w") as f:
-      f.write( f"""<?xml version="1.0" encoding="utf-8"?>
+   if not fn:
+     fn = "/tmp/" + vendor +"-"+ filetype +".xml"
+     if globs.verbosity:
+       print(f"create file: {fn}...")
+     if not force and os.path.isfile(fn):
+        raise Exception(f"File {fn} exists, already!")
+     with open(fn,"w") as f:
+        f.write( f"""<?xml version="1.0" encoding="utf-8"?>
 <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
  <mime-type type="application/x-{vendor}-{filetype}">
   <comment>{comment}</comment>
@@ -171,13 +195,21 @@ def main():
 
    if globs.interactive:
       interactiveParams()
+      
    if not globs.fileextension:
       print("createXfceMimeType " + __version__ + "." + __revision__)
       print(__doc__)
       sys.exit(1)
+   while globs.fileextension[0] == ".":
+      globs.fileextension=globs.fileextension[1:]
+   if not globs.filetype:
+      globs.filetype = globs.fileextension +"-file"
+   globs.filetype = globs.filetype.replace(" ","-")
 
-   ret = modMimelist(globs.apppath, f"application/x-{globs.vendor}-{globs.filetype}",
-                     globs.comment,  globs.force)
+   ret = 0
+   if globs.apppath:
+      ret = modMimelist(globs.apppath, f"application/x-{globs.vendor}-{globs.filetype}",
+                        globs.comment,  globs.force)
 
    if ret:   return ret
    return registerMimeType(globs.fileextension, globs.vendor, globs.filetype,
